8b95b86fbb926484e4f1c93ea1e6418e25965d28
Wa <122730917+STKR2@users.noreply.github.com>
2024-02-27 16:03:57 +0300

    Update pause.py

--- FallenMusic/Modules/pause.py
+++ FallenMusic/Modules/pause.py
@@ -22,12 +22,12 @@
 
 from pyrogram import filters
 from pyrogram.types import Message
-
+from FallenMusic.filters import command
 from FallenMusic import app, pytgcalls
 from FallenMusic.Helpers import admin_check, close_key, is_streaming, stream_off
 
 
-@app.on_message(filters.command(["pause"]) & filters.group)
+@app.on_message(command(["وقف"]) & filters.group)
 @admin_check
 async def pause_str(_, message: Message):
     try:
@@ -37,12 +37,12 @@ async def pause_str(_, message: Message):
 
     if not await is_streaming(message.chat.id):
         return await message.reply_text(
-            "ᴅɪᴅ ʏᴏᴜ ʀᴇᴍᴇᴍʙᴇʀ ᴛʜᴀᴛ ʏᴏᴜ ʀᴇsᴜᴍᴇᴅ ᴛʜᴇ sᴛʀᴇᴀᴍ ?"
+            "- لايوجد شي في الإتصال ."
         )
 
     await pytgcalls.pause_stream(message.chat.id)
     await stream_off(message.chat.id)
     return await message.reply_text(
-        text=f"➻ sᴛʀᴇᴀᴍ ᴩᴀᴜsᴇᴅ 🥺\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+        text=f"➻ تم التوقف بنجاح \n\n~ بواسطة : {message.from_user.mention} ",
         reply_markup=close_key,
     )
