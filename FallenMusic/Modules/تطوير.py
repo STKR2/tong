002cedba441f65c2c6377eb86cf920bf2b4ae4d3
Wa <122730917+STKR2@users.noreply.github.com>
2024-02-27 16:46:08 +0300

    Add files via upload

---
+++ FallenMusic/Modules/تطوير.py
@@ -0,0 +1,34 @@
+import asyncio
+import random
+import os
+import time
+import requests
+from random import  choice, randint
+from pyrogram import Client, filters
+from FallenMusic.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from FallenMusic import , app)
+
+
+@app.on_message(
+    command(["سورس","السورس","المطور"])
+    & filters.group
+)
+async def maker(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://graph.org/file/94fa4bb62424ea712eaa2.jpg",
+        caption=f"""-| مطور السورس \n-| قناة المطور""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "- مطور السورس .", url=f"https://t.me/T_5_G"),
+                ],
+                [
+                   InlineKeyboardButton(
+                        "- قناة المطور ", url=f"https://t.me/VVV5P"),
+                ],       
+            ]
+        ),
+    )
+
