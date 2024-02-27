29cd9759a877f63d75bbd04ef69a217e442f97e0
Wa <122730917+STKR2@users.noreply.github.com>
2024-02-27 16:48:28 +0300

    Update play.py

--- FallenMusic/Modules/play.py
+++ FallenMusic/Modules/play.py
@@ -179,7 +179,7 @@ async def play(_, message: Message):
         file_path = audio_dl(url)
     else:
         if len(message.command) < 2:
-            return await fallen.edit_text("» ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴɴᴀ ᴘʟᴀʏ ʙᴀʙʏ ?")
+            return await fallen.edit_text("- الرد على ملف صوتي او اعطاء شي للبحث .")
         await fallen.edit_text("- يجري معالجة البيانات .")
         query = message.text.split(None, 1)[1]
         try:
